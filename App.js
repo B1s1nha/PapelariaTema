9d77eebb78120188ff9bfc1da7a9bfe54461f2e4
Gabriella Huppes <b1s1nha_@hotmail.com>
2023-01-07 17:15:16 -0300

    feito o css

--- App.js
+++ App.js
@@ -4,44 +4,56 @@ function App() {
   const value = [
     {
       id: "e1",
-      title: "pen",
+      title: "Pen",
       amount: 1.5,
+      description: "Its a normal pen",
+      img: "https://media.gazetadopovo.com.br/2021/07/13102924/bic-cristal-design-divulgacao-960x540.jpg",
     },
     {
       id: "e2",
-      title: "pencil",
+      title: "Pencil",
       amount: 2,
+      description: "Its a normal pencil",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT06iym2PiLCMcIoY6H37vC6Mz8a3sd3HUhrA&usqp=CAU",
     },
     {
       id: "e3",
-      title: "eraser",
+      title: "Eraser",
       amount: 0.5,
+      description: "Its a normal eraser",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTty8YtJvtFZ7_7pK5Gkgldx0WbRPu8hdk02A&usqp=CAU",
     },
     {
       id: "e4",
-      title: "notebook",
+      title: "Notebook",
       amount: 10.5,
+      description: "Its a normal notebook",
+      img: "https://rukminim1.flixcart.com/image/416/416/kjq1mkw0-0/diary-notebook/8/i/g/eco-friendly-set-of-6-colorful-hi-quality-spiral-notebook-set-original-imafz88aa3zhuegb.jpeg?q=70",
     },
     {
       id: "e5",
-      title: "pancil case",
+      title: "Pencil Case",
       amount: 4,
+      description: "Its a normal pencil case",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYNghzEMtwrDhKxXsmLBvnlKkWyD0qIkfzMQ&usqp=CAU",
     },
     {
       id: "e6",
-      title: "stickers",
+      title: "Sticker",
       amount: 1.5,
+      description: "Its a normal sticker",
+      img: "https://cdn11.bigcommerce.com/s-miruu5f5ec/images/stencil/728x728/products/1219/8883/Line-Friends-in-the-Park-and-Flowery-Day-Themed-Stickers-Blind-Surprise-Pick-at-jellybeet.com-GS8701__99207.1654701121.jpg?c=2",
     },
   ];
   return (
     <div className="App">
       <h1>Paper Store</h1>
-      <ItemValue title={value[0].title} amount={value[0].amount}></ItemValue>
-      <ItemValue title={value[1].title} amount={value[1].amount}></ItemValue>
-      <ItemValue title={value[2].title} amount={value[2].amount}></ItemValue>
-      <ItemValue title={value[3].title} amount={value[3].amount}></ItemValue>
-      <ItemValue title={value[4].title} amount={value[4].amount}></ItemValue>
-      <ItemValue title={value[5].title} amount={value[5].amount}></ItemValue>
+      <ItemValue title={value[0].title} amount={value[0].amount} description={value[0].description} img={value[0].img}></ItemValue>
+      <ItemValue title={value[1].title} amount={value[1].amount} description={value[1].description} img={value[1].img}></ItemValue>
+      <ItemValue title={value[2].title} amount={value[2].amount} description={value[2].description} img={value[2].img}></ItemValue>
+      <ItemValue title={value[3].title} amount={value[3].amount} description={value[3].description} img={value[3].img}></ItemValue>
+      <ItemValue title={value[4].title} amount={value[4].amount} description={value[4].description} img={value[4].img}></ItemValue>
+      <ItemValue title={value[5].title} amount={value[5].amount} description={value[5].description} img={value[5].img}></ItemValue>
     </div>
   );
 }
--- components/ItemValue.js
+++ components/ItemValue.js
@@ -1,13 +1,23 @@
 import "./ItemValue.css";
 
-function ItemValue(props){
-
-    return (
-        <div>
-            <h2>{props.title}</h2>
-            <div>${props.amount}</div>
+function ItemValue(props) {
+  return (
+    <div className="item-value">
+      <div className="item-value__description">
+        <h2>
+          {props.title}
+          <br></br>
+          <div className="item-value__ds">{props.description}</div>
+          <div>
+          <br></br>
+          <img src={props.img} alt = "" height="320" width="420"></img>
         </div>
-    );
+        </h2>
+        
+        <div className="item-value__price">${props.amount}<div><br></br>BUY NOW</div></div>
+      </div>
+    </div>
+  );
 }
 
 export default ItemValue;
